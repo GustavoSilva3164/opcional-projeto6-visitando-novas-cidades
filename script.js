c077f8dd947d291aeeee79e524328c7dff60efe4
GuatavoSilva3164 <gustavosilva2008br@gmail.com>
2024-09-04 20:49:05 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+var touristName = prompt("Qual seu nome turista?");
+
+var visitedCities = "";
+var numberOfCities = 0;
+var response = prompt("Você visitou alguma cidade? (s/n)");
+
+while (response === 's') {
+    var city = prompt("Qual é o nome da cidade visitada?");
+    
+    if (numberOfCities === 0) {
+        visitedCities = city;
+    } else {
+        visitedCities += "\n- " + city;
+    }
+    
+    numberOfCities++;
+    
+    response = prompt("Você visitou alguma outra cidade? (s/n)");
+}
+
+console.log("Turista: " + touristName);
+console.log("Quantidade de cidades visitadas: " + numberOfCities);
+
+if (numberOfCities > 0) {
+    console.log("Cidades visitadas:\n" + visitedCities);
+} else {
+    console.log("Nenhuma cidade visitada.");
+}
